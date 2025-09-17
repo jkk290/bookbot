aa8e08925e2f6c1e24fed34d04c2132b1b79f02d
jkk290 <jared@kuromoto.dev>
2025-09-16 19:16:31 -1000

    Update main to run with path argument

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_word_count, get_character_count, get_sorted
 
 def get_book_text(path_to_file):
@@ -6,13 +7,15 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
-    path_to_file = 'books/frankenstein.txt'
-    frankenstein = get_book_text(path_to_file)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    frankenstein = get_book_text(sys.argv[1])
     frankenstein_words = get_word_count(frankenstein)
     frankenstein_character_counts = get_character_count(frankenstein)
     sorted_word_counts = get_sorted(frankenstein_character_counts)
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {path_to_file}...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {frankenstein_words} total words")
     print(f"--------- Character Count -------")
